3d02ac17d82692f0280df6cc6f15da8f899c4ca8
Johan Rocha <johanmarcos50@gmail.com>
2024-03-07 01:59:29 -0300

    fix: button can't be in 'a' tag

--- src/components/Navbar/BlueButton.tsx
+++ src/components/Navbar/BlueButton.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 export default function BlueButton({
   children,
+  onClick,
 }: {
   children: React.ReactNode;
+  onClick: () => void;
 }) {
   return (
-    <button className="rounded-full py-2 px-6 bg-gradient-to-r from-[#1C2337] to-[#5C86F2]">
-      <a className="text-white hover:text-stone-300">{children}</a>
+    <button onClick={onClick} className="rounded-full py-2 px-6 bg-gradient-to-r from-[#1C2337] to-[#5C86F2]">
+      <span className="text-white hover:text-stone-300">{children}</span>
     </button>
-  );
+  ); //fixed hydratation code -> button can't be in 'a' tag
 }
