c7f1a924ef463ad94e3b1286f03064d845345740
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2024-03-10 18:18:44 -0300

    refactor: new postcard struct

--- src/components/Posts/PostCard.tsx
+++ src/components/Posts/PostCard.tsx
@@ -18,27 +18,34 @@ export default async function PostCard({ post }: { post: Post }) {
   return (
     <Link
       href={`/posts/${post.id}`}
-      className="rounded-lg shadow-lg overflow-hidden w-[390px] h-fit"
+      className="flex flex-col justify-between rounded-lg shadow-2xl overflow-hidden w-[390px] h-fill "
     >
-      <div className="relative h-52">
-        <Image
-          src={post.image}
-          alt={post.title}
-          fill
-          className="z-0 max-h-52 object-cover object-center"
-        />
-      </div>
-      <div className="p-6 flex flex-col justify-between gap-4 h-full">
-        <div className="space-y-4">
+      <div>
+        <div className="relative h-52">
+          <Image
+            src={post.image}
+            alt={post.title}
+            fill
+            className="z-0 max-h-52 object-cover object-center"
+          />
+        </div>
+
+        <div className="p-6 w-full h-fit space-y-4">
           <span
             key={post.tags[0]}
             className="inline-block bg-[#7D9746] rounded-sm px-3 py-1 text-sm font-normal text-white"
           >
             {post.tags[0]}
           </span>
-          <h2 className="text-2xl font-bold">{post.title}</h2>
-          <p className="mt-4 text-gray-600 h-fill">{post.description}</p>
+          <h2 className="text-2xl font-bold text-left whitespace-normal">
+            {post.title}
+          </h2>
+          <p className="mt-4 text-gray-600 line-clamp-3 whitespace-normal">
+            {post.description}
+          </p>
         </div>
+      </div>
+      <div className="p-6 flex flex-col justify-between gap-4 h-fill">
         <div className="flex flex-row items-center justify-between w-full">
           <div className="flex flex-row items-center gap-2 text-zinc-600 ">
             <Avatar className="shadow-lg">
