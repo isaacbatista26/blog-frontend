a7965750da5ddf3e16887408abbe50636eca3e82
isaacbatista26 <isaacbatista2266@gmail.com>
2024-03-10 20:39:17 -0300

    feat: Section Participantes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Apoio from "@/components/Apoio";
 import Coleta from "@/components/Inicio/Coleta";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
+import Participantes from "@/components/participantes/participantes";
+
 export default function Home() {
   return (
     <div className="relative">
@@ -12,6 +14,7 @@ export default function Home() {
       <Sobre />
       <Coleta />
       <Apoio />
+      <Participantes />
       <Footer />
     </div>
   );
