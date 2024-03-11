bbe0b165607c29576794a021e30f7b0127c7df33
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2024-03-11 14:30:48 -0300

    feat: loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,9 @@
 export default function Loading() {
-  // Or a custom loading skeleton component
-  return <></>;
+  return (
+    <div className="">
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-zinc-500"></div>
+      </div>
+    </div>
+  );
 }
